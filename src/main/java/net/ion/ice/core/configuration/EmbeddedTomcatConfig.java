9c06ca6cfc0b4d7760db0822137ac7851d3b3d5a
juneyoungoh <jyoh@i-on.net>
2017-06-22 15:53:58 +0900

    톰캣 엑세스 로그 로그스태시

--- src/main/java/net/ion/ice/core/configuration/EmbeddedTomcatConfig.java
+++ src/main/java/net/ion/ice/core/configuration/EmbeddedTomcatConfig.java
@@ -1,5 +1,8 @@
 package net.ion.ice.core.configuration;
 
+import ch.qos.logback.access.tomcat.LogbackValve;
+import net.logstash.logback.appender.LogstashAccessTcpSocketAppender;
+import net.logstash.logback.encoder.LogstashAccessEncoder;
 import org.apache.catalina.valves.AccessLogValve;
 import org.apache.log4j.Logger;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
@@ -14,17 +17,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EmbeddedTomcatConfig implements EmbeddedServletContainerCustomizer {
     private Logger logger = Logger.getLogger(EmbeddedTomcatConfig.class);
+    private boolean useLogbackValve = false;
+
+    public void ableLogbackValve(boolean use){
+        this.useLogbackValve = use;
+    }
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
         try{
             if (container instanceof TomcatEmbeddedServletContainerFactory) {
                 TomcatEmbeddedServletContainerFactory factory = (TomcatEmbeddedServletContainerFactory) container;
-                AccessLogValve accessLogValve = new AccessLogValve();
-                accessLogValve.setDirectory("/resource/ice2/tomcat/access-logs");
-                accessLogValve.setPattern("common");
-                accessLogValve.setSuffix(".log");
-                factory.addContextValves(accessLogValve);
+                if(!useLogbackValve) {
+                    AccessLogValve accessLogValve = new AccessLogValve();
+                    accessLogValve.setDirectory("/resource/ice2/tomcat/access-logs");
+                    accessLogValve.setPattern("common");
+                    accessLogValve.setSuffix(".log");
+                    factory.addContextValves(accessLogValve);
+                } else {
+                    LogbackValve logbackValve = new LogbackValve();
+                    LogstashAccessTcpSocketAppender logstashAccessAppender = new LogstashAccessTcpSocketAppender();
+                    logstashAccessAppender.addDestination("125.131.88.156:5000");
+                    logstashAccessAppender.setEncoder(new LogstashAccessEncoder());
+
+                    logbackValve.addAppender(logstashAccessAppender);
+                    factory.addContextValves(logbackValve);
+                }
+
             } else {
                 logger.error("WARNING! this customizer does not support your configured container");
             }
