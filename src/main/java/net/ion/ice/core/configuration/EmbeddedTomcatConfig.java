b916da22f27bcd472779ea1fc569b88d78023702
juneyoungoh <jyoh@i-on.net>
2017-06-26 11:53:06 +0900

    no context given 수정

--- src/main/java/net/ion/ice/core/configuration/EmbeddedTomcatConfig.java
+++ src/main/java/net/ion/ice/core/configuration/EmbeddedTomcatConfig.java
@@ -1,10 +1,13 @@
 package net.ion.ice.core.configuration;
 
 import ch.qos.logback.access.tomcat.LogbackValve;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import net.logstash.logback.appender.LogstashAccessTcpSocketAppender;
 import net.logstash.logback.encoder.LogstashAccessEncoder;
+import net.logstash.logback.encoder.com.lmax.disruptor.dsl.ProducerType;
 import org.apache.catalina.valves.AccessLogValve;
-import org.apache.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -16,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class EmbeddedTomcatConfig implements EmbeddedServletContainerCustomizer {
-    private Logger logger = Logger.getLogger(EmbeddedTomcatConfig.class);
+    private org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(EmbeddedTomcatConfig.class);
     private boolean useLogbackValve = true;
 
     public void ableLogbackValve(boolean use){
@@ -28,12 +31,22 @@ public class EmbeddedTomcatConfig implements EmbeddedServletContainerCustomizer 
         try{
             if (container instanceof TomcatEmbeddedServletContainerFactory) {
                 TomcatEmbeddedServletContainerFactory factory = (TomcatEmbeddedServletContainerFactory) container;
+
+                //logstash appender
                 if(useLogbackValve) {
+                    Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+                    LoggerContext loggerContext = rootLogger.getLoggerContext();
+
                     LogbackValve logbackValve = new LogbackValve();
                     LogstashAccessTcpSocketAppender logstashAccessAppender = new LogstashAccessTcpSocketAppender();
+
+                    logstashAccessAppender.setName("access-logstash");
+                    logstashAccessAppender.setContext(loggerContext);
                     logstashAccessAppender.addDestination("125.131.88.156:5001");
+
                     LogstashAccessEncoder lae = new LogstashAccessEncoder();
                     lae.setWriteVersionAsString(true);
+
                     logstashAccessAppender.setEncoder(lae);
                     logbackValve.addAppender(logstashAccessAppender);
                     logbackValve.setAsyncSupported(true);
@@ -41,6 +54,8 @@ public class EmbeddedTomcatConfig implements EmbeddedServletContainerCustomizer 
                     factory.addContextValves(logbackValve);
                 }
 
+
+                // file appender
                 AccessLogValve accessLogValve = new AccessLogValve();
                 accessLogValve.setDirectory("/resource/ice2/tomcat/access-logs");
                 accessLogValve.setPattern("common");
