7a3c3ba3ff77fec38b888cc6515bd6525e717dbb
jjd <jjd929@i-on.net>
2017-06-29 22:59:00 +0900

    Tree 검색시 검색 가능.

--- src/main/java/net/ion/ice/core/infinispan/InfinispanRepositoryService.java
+++ src/main/java/net/ion/ice/core/infinispan/InfinispanRepositoryService.java
@@ -133,6 +133,7 @@ public class InfinispanRepositoryService {
             if(pt.isTreeable()){
                 QueryContext subQueryContext = QueryContext.makeQueryContextForTree(nodeType, pt, "root") ;
                 subQueryContext.setTreeable(true);
+                subQueryContext.getQueryTerms().addAll(queryContext.getQueryTerms());
                 List<Node> result = getSubQueryNodes(pt.getReferenceType(), subQueryContext) ;
                 for(Node node : result){
                     node.toDisplay();
