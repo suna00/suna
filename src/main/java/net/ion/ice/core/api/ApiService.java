84efad291f71ca08c12dcc7e72aa83953dc0e58a
jaeho <pm1200@i-on.net>
2017-07-05 17:20:45 +0900

    api 쿼리

--- src/main/java/net/ion/ice/core/api/ApiService.java
+++ src/main/java/net/ion/ice/core/api/ApiService.java
@@ -29,6 +29,7 @@ public class ApiService {
 
         if(apiMethod.equals("POST")){
             if(request instanceof MultipartHttpServletRequest) {
+                ApiContext context = ApiContext.createContext(apiNode, request.getParameterMap(), ((MultipartHttpServletRequest) request).getMultiFileMap()) ;
                 return JsonResponse.create(nodeService.saveNode(request.getParameterMap(), ((MultipartHttpServletRequest) request).getMultiFileMap(), "")) ;
             }
             return JsonResponse.create(nodeService.saveNode(request.getParameterMap(), "")) ;
