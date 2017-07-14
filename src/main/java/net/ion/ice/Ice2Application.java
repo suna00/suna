b057a28811f5c9dab2141e868047178b0b606a85
juneyoungoh <jyoh@i-on.net>
2017-07-14 18:21:49 +0900

    devtools 가 배포에 오류나나

--- src/main/java/net/ion/ice/Ice2Application.java
+++ src/main/java/net/ion/ice/Ice2Application.java
@@ -18,7 +18,7 @@ public class Ice2Application {
 	public static final String USE_HAZELCAST = "true";
 
 	public static void main(String[] args) {
-	    System.setProperty("spring.devtools.restart.enabled","false");
+//	    System.setProperty("spring.devtools.restart.enabled","false");
 //        System.setProperty("spring.devtools.livereload.enabled","true");
         Stagemonitor.init();
 		ApplicationContext ctx =  SpringApplication.run(Ice2Application.class, args);
