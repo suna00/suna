e3b5632a0ff64a5ad5a941d4569d35ee58b0d860
hwyoon99 <hwyoon99@mail.i-on.net>
2017-08-09 23:16:21 +0900

    [hwyoon][CJMwave] 음... 바뀐것같은데 안바뀌네....

--- src/main/java/net/ion/ice/core/event/TreeService.java
+++ src/main/java/net/ion/ice/core/event/TreeService.java
@@ -9,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.stagemonitor.util.StringUtils;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by jaeho on 2017. 7. 11..
@@ -41,16 +39,93 @@ public class TreeService {
 
     public void sortEvent(ExecuteContext context){
 
-        Map<String, Object> _data = new HashMap<>();
-        _data = context.getData();
+        Map<String, Object> _data = context.getData();
 
-        //Node node = context.getNode() ;
-        //changeEvent(node, "change");
+        String dragKey = _data.get("dragKey").toString();
+        String dropKey = _data.get("dropKey").toString();
+        String dropGap = _data.get("dropGap").toString();
+
+        Node dragNode = nodeService.read("contsCtgry", dragKey);
+        Node gropNode = nodeService.read("contsCtgry", dropKey);
+
+        int dropOrder = Integer.parseInt(gropNode.getValue("sortOdrg").toString());
+
+        String upperId = ( dropGap.equals("true") ) ? gropNode.getValue("upperContsCtgryId").toString() : gropNode.getId().toString();
+        List<Node> upNodeList = nodeService.getNodeList("contsCtgry", "upperContsCtgryId_matching="+upperId);
+
+        if(upNodeList.size()>0){
+            dragNode.put("upperContsCtgryId",upperId);
+
+            if(dropGap.equals("false")){
+                Collections.sort(upNodeList,new CompareSeqDesc());  //int로 내림차순
+                upNodeList.add(dragNode);
+            }else{
+                if(upperId.equals(dragNode.getValue("upperContsCtgryId").toString())){
+                    upNodeList.remove(dragNode);
+                }
+                Collections.sort(upNodeList,new CompareSeqAsc()); //int로 오름차순
+                int idx = 0;
+                for(int i=0; i<upNodeList.size(); i++){
+                    Node node = upNodeList.get(i);
+                    int nodeOrder = Integer.parseInt(node.getValue("sortOdrg").toString());
+                    if(nodeOrder == dropOrder){
+                        idx = i;
+                        break;
+                    }
+                }
+                upNodeList.add(idx,dragNode);   //내가 중간에 들어가고 나머지가 뒤로 밀리는게 맞겠지??
+            }
+
+            //새 upNodeList for문 돌면서 전부 인덱스값으로 sortOdrg 계속 put한다
+            for(int j=1; j<=upNodeList.size(); j++){
+                upNodeList.get(j).put("sortOdrg",j);
+            }
+
+        }else if(dropGap.equals("false") && upNodeList.size() <=0){
+            dragNode.put("upperContsCtgryId",upperId);
+            dragNode.put("sortOdrg",1);
+        }
+
+        changeEvent(dragNode);
+
     }
 
-    private String changeEvent(Node node, String event) {
-        Node newNode = node;
-        return "SUCCESS";
+    private Node changeEvent(Node node) {
+        Node dragNode = node;
+        return dragNode;
+    }
+
+
+    /**
+     * int로 내림차순(Desc) 정렬
+     * @author Administrator
+     *
+     */
+    static class CompareSeqDesc implements Comparator<Node>{
+        @Override
+        public int compare(Node o1, Node o2) {
+            // TODO Auto-generated method stub
+            int o1Order = Integer.parseInt(o1.getValue("sortOdrg").toString());
+            int o2Order = Integer.parseInt(o2.getValue("sortOdrg").toString());
+            return o1Order > o2Order ? -1 : o1Order < o2Order ? 1:0;
+        }
+    }
+
+    /**
+     * int로 오름차순(Asc) 정렬
+     * @author Administrator
+     *
+     */
+    static class CompareSeqAsc implements Comparator<Node>{
+        @Override
+        public int compare(Node o1, Node o2) {
+            // TODO Auto-generated method stub
+            int o1Order = Integer.parseInt(o1.getValue("sortOdrg").toString());
+            int o2Order = Integer.parseInt(o2.getValue("sortOdrg").toString());
+            return o1Order < o2Order? -1 : o1Order > o2Order ? 1:0;
+        }
     }
 
 }
+
+
