cb10ab9e5089cdcbe8360eb27d28afd8f4d34002
jaeho <pm1200@i-on.net>
2017-08-22 17:58:14 +0900

    code Filter 검색 기능 추가

--- src/main/java/net/ion/ice/core/query/QueryUtils.java
+++ src/main/java/net/ion/ice/core/query/QueryUtils.java
@@ -174,7 +174,15 @@ public class QueryUtils {
         PropertyType propertyType = (PropertyType) nodeType.getPropertyType(fieldId);
         if (propertyType != null && propertyType.isIndexable()) {
             try {
-                return new QueryTerm(fieldId, propertyType.getLuceneAnalyzer(), method, value, propertyType.getValueType());
+                if(StringUtils.isNotEmpty(propertyType.getCodeFilter())){
+                    if(StringUtils.contains(value, ">")) {
+                        return new QueryTerm(fieldId, propertyType.getLuceneAnalyzer(), method, value, propertyType.getValueType());
+                    }else{
+                        return new QueryTerm(fieldId, propertyType.getLuceneAnalyzer(), method, propertyType.getCodeFilter() + ">" + value, propertyType.getValueType());
+                    }
+                }else {
+                    return new QueryTerm(fieldId, propertyType.getLuceneAnalyzer(), method, value, propertyType.getValueType());
+                }
             }catch (Exception e){
                 return  null ;
             }
