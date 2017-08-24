0f58a4def7bbdc04a79c7ee60dec8ab771100c78
pm1200 <pm1200@i-on.net>
2017-08-24 12:46:26 +0900

    파일 관련 수정 중

--- src/main/java/net/ion/ice/core/node/Node.java
+++ src/main/java/net/ion/ice/core/node/Node.java
@@ -366,7 +366,6 @@ public class Node implements Map<String, Object>, Serializable, Cloneable{
                 remove(pt.getPid()) ;
             }
         }
-//        properties.toStore();
         return this ;
     }
 
