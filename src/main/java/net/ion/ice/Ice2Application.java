b6c70bf239abe1865a9f4fbc306423f815e97f88
juneyoungoh <xzawed85>
2017-08-26 16:07:39 +0900

    배포 설정 추가 (Ice2Application.java)

--- src/main/java/net/ion/ice/Ice2Application.java
+++ src/main/java/net/ion/ice/Ice2Application.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -33,4 +34,11 @@ public class Ice2Application extends SpringBootServletInitializer {
 		taskExecutor.setQueueCapacity(100);
 		return taskExecutor;
 	}
+
+	// refers https://www.mkyong.com/spring-boot/spring-boot-deploy-war-file-to-tomcat/
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return super.configure(builder);
+        return builder.sources(Ice2Application.class);
+    }
 }
