544285f98fd644e8d5f77504c504afa0ef8140b3
hwyoon99 <hwyoon99@mail.i-on.net>
2017-08-28 04:58:45 +0900

    [hwyoon][CJMwave] 데이터 저장하는거 아직도 모르겠음...

--- src/main/java/net/ion/ice/cjmwave/vote/VoteService.java
+++ src/main/java/net/ion/ice/cjmwave/vote/VoteService.java
@@ -3,11 +3,11 @@ package net.ion.ice.cjmwave.vote;
 import net.ion.ice.core.context.ExecuteContext;
 import net.ion.ice.core.event.EventBroker;
 import net.ion.ice.core.infinispan.InfinispanRepositoryService;
-import net.ion.ice.core.json.JsonUtils;
 import net.ion.ice.core.node.NodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -37,15 +37,21 @@ public class VoteService {
     @Autowired
     private EventBroker eventBroker ;
 
-    public void selCreateEvent(ExecuteContext context){
+    public void selCreateEvent(ExecuteContext context) throws IOException {
 
         Map<String, Object> _data = context.getData();
 
         //여기다 이제 투표기본정보, 제한, 항목 노드에 각각 저장을 .....
+        //nodeService.saveNode(_data);
 
-        Object voteInfo = JsonUtils.parsingJsonToObject(_data.get("voteInfo").toString());
-        Object termRstrtnInfo = JsonUtils.parsingJsonToObject(_data.get("termRstrtnInfo").toString());
-        Object voteItems = JsonUtils.parsingJsonToObject(_data.get("voteItems").toString());
+        //Map<String, Object> voteInfo = JsonUtils.parsingJsonToMap(_data.get("voteInfo").toString());
+
+        /*Map<String, Object> voteInfo = JsonUtils.parsingJsonToMap(_data.get("voteInfo").toString());
+
+        //MultiValueMap<String, MultipartFile> multiFileMap =
+
+        Map<String, Object> termRstrtnInfo = JsonUtils.parsingJsonToMap(_data.get("termRstrtnInfo").toString());
+        Map<String, Object> voteItems = JsonUtils.parsingJsonToMap(_data.get("voteItems").toString());*/
 
     }
 
