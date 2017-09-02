177469989b42ba5be6ccc7b1745d882c59a47124
jjong <jlee84@i-on.net>
2017-09-02 20:01:04 +0900

    장바구니 담기 api : 장바구니 cart, cartproduct, cartproductitem 에 참조형 필드 제거 (price, name 등)

--- src/main/java/net/ion/ice/service/CartService.java
+++ src/main/java/net/ion/ice/service/CartService.java
@@ -1,11 +1,18 @@
 package net.ion.ice.service;
 
 import net.ion.ice.core.context.ExecuteContext;
+import net.ion.ice.core.data.bind.NodeBindingInfo;
+import net.ion.ice.core.data.bind.NodeBindingService;
+import net.ion.ice.core.json.JsonUtils;
 import net.ion.ice.core.node.Node;
 import net.ion.ice.core.node.NodeService;
+import net.ion.ice.core.node.NodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Service("cartService")
@@ -19,11 +26,38 @@ public class CartService {
     @Autowired
     private NodeService nodeService ;
 
-    public void addCart(ExecuteContext context){
+    public void addCart(ExecuteContext context) throws IOException {
         Map<String, Object> data = context.getData();
+        Object cartId = data.get("cartId");
+        Node node = null;
 
-        nodeService.executeNode(data, context.getNode().getTypeId(), CREATE) ;    // goods, securities
-        nodeService.executeNode(data, "product", CREATE) ;
+        if(cartId != null){
+            node = NodeUtils.getNode("cart", cartId.toString());
+        }
+
+        if(node == null){
+            node = nodeService.executeNode(data, "cart", CREATE) ;
+            data.put("cartId", node.getId());
+            List<Node> baseOptionlist = createList(data,"cartProduct");
+            data.put("cartProductId", baseOptionlist.get(0).getId());
+            List<Node> addOptionList = createList(data,"cartProductItem");
+
+
+
+//            nodeBindingService.save(data, context.getNodeType().getTypeId());
+
+//            NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo(context.getNodeType().getTypeId()) ;
+//            nodeBindingInfo.getJdbcTemplate().update();
+
+
+
+//            node = nodeService.executeNode(data, "cart", CREATE) ;
+//            data.put("cartId", node.getId());
+//            createList(data,"cartProduct");
+        }else{
+            node = nodeService.executeNode(data, "cart", UPDATE) ;
+
+        }
 
         try {
 
@@ -33,6 +67,16 @@ public class CartService {
 
     }
 
+
+    private List<Node> createList(Map<String, Object> data, String tid) throws IOException {
+        List<Node> list = new ArrayList<>();
+        for(Map<String, Object> map : JsonUtils.parsingJsonToList(data.get(tid).toString())){
+            map.putAll(data);
+            Node node = nodeService.executeNode(map, tid, CREATE) ;
+            list.add(node);
+        }
+        return list;
+    }
 
     public void update(ExecuteContext context){
         Node node = context.getNode() ;
