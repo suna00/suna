8caa2dd486c447986b03f91aea8c3be8a5dcf084
jjong <jlee84@i-on.net>
2017-09-02 21:59:09 +0900

    장바구니 담기 ( 장바구니 생성, 장바구니 아이템 추가 처리 )

--- src/main/java/net/ion/ice/service/CartService.java
+++ src/main/java/net/ion/ice/service/CartService.java
@@ -1,11 +1,13 @@
 package net.ion.ice.service;
 
 import net.ion.ice.core.context.ExecuteContext;
+import net.ion.ice.core.context.QueryContext;
 import net.ion.ice.core.data.bind.NodeBindingInfo;
 import net.ion.ice.core.data.bind.NodeBindingService;
 import net.ion.ice.core.json.JsonUtils;
 import net.ion.ice.core.node.Node;
 import net.ion.ice.core.node.NodeService;
+import net.ion.ice.core.node.NodeType;
 import net.ion.ice.core.node.NodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static net.ion.ice.core.node.NodeUtils.getNodeBindingService;
 
 @Service("cartService")
 public class CartService {
@@ -36,43 +40,60 @@ public class CartService {
         }
 
         if(node == null){
-            node = nodeService.executeNode(data, "cart", CREATE) ;
-            data.put("cartId", node.getId());
-            List<Node> baseOptionlist = createList(data,"cartProduct");
-            data.put("cartProductId", baseOptionlist.get(0).getId());
-            List<Node> addOptionList = createList(data,"cartProductItem");
-
-
-
-//            nodeBindingService.save(data, context.getNodeType().getTypeId());
-
-//            NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo(context.getNodeType().getTypeId()) ;
-//            nodeBindingInfo.getJdbcTemplate().update();
-
-
-
-//            node = nodeService.executeNode(data, "cart", CREATE) ;
-//            data.put("cartId", node.getId());
-//            createList(data,"cartProduct");
+            newCart(data);
         }else{
-            node = nodeService.executeNode(data, "cart", UPDATE) ;
-
-        }
-
-        try {
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            nodeService.executeNode(data, "cart", UPDATE) ;
+            mergeList(data, cartId, "cartProduct");
         }
 
     }
 
+    private void mergeList(Map<String, Object> data, Object cartId, String tid) throws IOException {
+        NodeType nodeType = NodeUtils.getNodeType(tid);
+        QueryContext queryContext = QueryContext.createQueryContextFromText("cartId_matching="+cartId, nodeType);
+        List<Map<String, Object>> referenced = getNodeBindingService().getNodeBindingInfo(nodeType.getTypeId()).list(queryContext);
 
-    private List<Node> createList(Map<String, Object> data, String tid) throws IOException {
+        List<Map<String, Object>> maps = JsonUtils.parsingJsonToList(data.get(tid).toString());
+
+        boolean exist = false;
+        for(Map<String, Object> map : maps){
+            for(Map<String, Object> obj : referenced){
+                if((map.get("baseOptionItemId")).equals(obj.get("baseOptionItemId"))){
+                    exist = true;
+                }
+            }
+            if(!exist){
+                map.putAll(data);
+                nodeService.executeNode(map, tid, CREATE) ;
+            }else{
+                // 장바구니에 이미 존재하는 상품입니다
+            }
+        }
+
+    }
+
+    private void newCart(Map<String, Object> data) throws IOException {
+        Node node = nodeService.executeNode(data, "cart", CREATE) ;
+        data.put("cartId", node.getId());
+
+        createList(data);
+
+    }
+
+
+    private List<Node> createList(Map<String, Object> data) throws IOException {
         List<Node> list = new ArrayList<>();
-        for(Map<String, Object> map : JsonUtils.parsingJsonToList(data.get(tid).toString())){
+        for(Map<String, Object> map : JsonUtils.parsingJsonToList(data.get("cartProduct").toString())){
             map.putAll(data);
-            Node node = nodeService.executeNode(map, tid, CREATE) ;
+            Node node = nodeService.executeNode(map, "cartProduct", CREATE) ;
+            map.put("cartProductId", node.getId());
+
+            if(map.get("cartProductItem") != null){
+                for(Map<String, Object> item : (List<Map<String, Object>>) map.get("cartProductItem")){
+                    item.putAll(map);
+                    nodeService.executeNode(item, "cartProductItem", CREATE) ;
+                }
+            }
             list.add(node);
         }
         return list;
