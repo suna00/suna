888dc43a0d2da8ed6978d4aa07c0f4379efe52aa
leehh <leehh0713>
2017-09-10 00:16:41 +0900

    [leehh][CJMwave]api 수정

--- src/main/java/net/ion/ice/cjmwave/pip/VideoService.java
+++ src/main/java/net/ion/ice/cjmwave/pip/VideoService.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,42 +19,56 @@ import java.util.Map;
 @Service("videoService")
 public class VideoService {
     private String[] authKey = {"smccjenm"};
-    private String[] cdnDomainUrl = {"pip-vodhls-mwave.cjenm.skcdn.com","pip-cliphls-mwave.cjenm.skcdn.com"};
+    private String[] cdnDomainUrl = {"pip-vodhls-mwave.cjenm.skcdn.com", "pip-cliphls-mwave.cjenm.skcdn.com"};
+    private String[] buildPids = {"mediaUrl", "contentImgUrl", "subtitlePath"};
 
-    public void getVideoCdnUrl(ExecuteContext context){
-        Map<String, Object> returnResult = null;
-        String cdmDomainUrlStr = "";
+    public void getVideoCdnUrl(ExecuteContext context) {
+        Map<String, String> returnResult = new HashMap<>();
+        String cdmDomainUrlStr;
+
         Map<String, Object> data = context.getData();
-        String contentId = data.get("contentId").toString();
+        if (data.isEmpty()) throw new IceRuntimeException("Parameter is Empty");
 
-        if (data == null || StringUtils.isEmpty(contentId)) {
-            throw new IceRuntimeException("contentId Parameter is Null") ;
+        String contentId = data.get("contentId").toString();
+        if (StringUtils.isEmpty(contentId)) {
+            throw new IceRuntimeException("contentId Parameter is Null");
         }
 
         Node node = NodeUtils.getNode("pgmVideo", contentId);
-        if (node == null) {
-            throw new IceRuntimeException("Node is Null : contentId="+contentId) ;
+        if (node.isEmpty()) {
+            throw new IceRuntimeException("Node is Null : contentId=" + contentId);
         }
 
-        String clipType = node.getStringValue("clipType");
-        String mediaUrl = node.getStringValue("mediaUrl");
-        if("1".equals(clipType)){
+        String clipTypeObj = node.getStringValue("clipType");
+        String clipType = (StringUtils.contains(clipTypeObj, Node.ID_SEPERATOR)) ? StringUtils.substringAfterLast(clipTypeObj, Node.ID_SEPERATOR) : clipTypeObj;
+        String playTime = node.getStringValue("playTime");
+        returnResult.put("playTime",playTime);
+
+        if ("1".equals(clipType)) {
             cdmDomainUrlStr = cdnDomainUrl[0];
-        }else if("2".equals(clipType)){
+        } else if ("2".equals(clipType)) {
             cdmDomainUrlStr = cdnDomainUrl[1];
-        }else{
+        } else {
             cdmDomainUrlStr = "";
         }
 
-        SkbURI uri = new SkbURI.Builder().sharedKey(authKey[0]).mediaUrl(mediaUrl).build();
+        for (String pid : buildPids) {
+            String pidValue = node.getStringValue(pid);
+            if (StringUtils.isNotEmpty(pidValue)) {
+                SkbURI uri = new SkbURI.Builder().sharedKey(authKey[0]).mediaUrl(pidValue).build();
 
-        try {
-            returnResult.put("mediaUrl",cdmDomainUrlStr+uri.getURI());
-            System.out.println("##PIP CDN URL : "+cdmDomainUrlStr+uri.getURI());
-        }catch (IllegalArgumentException iae){
-            iae.printStackTrace();
-        }catch (URISyntaxException use){
-            use.printStackTrace();
+                try {
+                    String uriStri = cdmDomainUrlStr + uri.getURI();
+                    System.out.println("##PIP " + pid + " CDN URL : " + uriStri);
+                    returnResult.put(pid, uriStri);
+                } catch (IllegalArgumentException iae) {
+                    iae.printStackTrace();
+                } catch (URISyntaxException use) {
+                    use.printStackTrace();
+                }
+            } else {
+                returnResult.put(pid, "");
+            }
         }
 
         context.setResult(returnResult);
