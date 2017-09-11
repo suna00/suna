fd9b543b0b7fcdff4db80de2b027dbf9756e598e
Jinwoong Han <theskyend0@i-on.net>
2017-09-11 21:20:47 +0900

    file response 수정

--- src/main/java/net/ion/ice/core/file/FileController.java
+++ src/main/java/net/ion/ice/core/file/FileController.java
@@ -5,6 +5,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,26 +19,48 @@ public class FileController {
     @Autowired
     private FileService fileService ;
 
+
+
     @GetMapping("/file/{tid}/{pid}/{year}/{day}/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String tid, @PathVariable String pid, @PathVariable String year, @PathVariable String day, @PathVariable String filename) {
 
         Resource file = fileService.loadAsResource(tid, pid, tid + "/" + pid + "/" +  year + "/" + day + "/" + filename);
+        return getAttach(filename, file);
+    }
+
+    private ResponseEntity<Resource> getAttach(@PathVariable String filename, Resource file) {
         return ResponseEntity
                 .ok()
+                .header(HttpHeaders.CONTENT_TYPE, FileUtils.getContentType(filename))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
+                .body(file);
+    }
+
+    @GetMapping("/image/{tid}/{pid}/{year}/{day}/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveImage(@PathVariable String tid, @PathVariable String pid, @PathVariable String year, @PathVariable String day, @PathVariable String filename) {
+        Resource file = fileService.loadAsResource(tid, pid, tid + "/" + pid + "/" +  year + "/" + day + "/" + filename);
+        return getNonAttach(filename, file);
+    }
+
+    private ResponseEntity<Resource> getNonAttach(@PathVariable String filename, Resource file) {
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.CONTENT_TYPE, FileUtils.getContentType(filename))
                 .body(file);
     }
 
     @GetMapping("/{tid}/{pid}/{year}/{day}/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile2(@PathVariable String tid, @PathVariable String pid, @PathVariable String year, @PathVariable String day, @PathVariable String filename) {
-
         Resource file = fileService.loadAsResource(tid, pid, tid + "/" + pid + "/" +  year + "/" + day + "/" + filename);
-        return ResponseEntity
-                .ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
-                .body(file);
+        if(FileUtils.isAttach(file.getFilename())){
+            return getAttach(filename, file);
+        }else{
+            return getNonAttach(filename, file) ;
+        }
+
     }
 
 }
