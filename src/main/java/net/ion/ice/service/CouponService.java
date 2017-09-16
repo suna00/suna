027224b4137f43cb05078d33d53e847dfe6cbab0
jjong <jlee84@i-on.net>
2017-09-16 23:10:16 +0900

    적용 가능한 쿠폰 ing...

--- src/main/java/net/ion/ice/service/CouponService.java
+++ src/main/java/net/ion/ice/service/CouponService.java
@@ -1,16 +1,20 @@
 package net.ion.ice.service;
 
 import net.ion.ice.core.context.ExecuteContext;
+import net.ion.ice.core.data.DBService;
+import net.ion.ice.core.data.DBUtils;
+import net.ion.ice.core.data.bind.NodeBindingInfo;
+import net.ion.ice.core.data.bind.NodeBindingService;
 import net.ion.ice.core.node.Node;
 import net.ion.ice.core.node.NodeService;
+import net.ion.ice.core.node.NodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service("couponService")
 public class CouponService {
@@ -23,8 +27,11 @@ public class CouponService {
 
     @Autowired
     private NodeService nodeService ;
+    @Autowired
+    private NodeBindingService nodeBindingService ;
     protected CommonService common;
 
+    private JdbcTemplate jdbcTemplate ;
 
     public ExecuteContext download(ExecuteContext context) {
         Map<String, Object> data = new LinkedHashMap<>(context.getData());
@@ -99,4 +106,79 @@ public class CouponService {
         return context;
     }
 
+//    적용가능한
+    public ExecuteContext applicable(ExecuteContext context) {
+        Map<String, Object> data = new LinkedHashMap<>(context.getData());
+
+        if(data.get("memberNo") == null) return context;
+
+        String[] params = { "tempOrderId" };
+        if (common.requiredParams(context, data, params)) return context;
+
+        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo(context.getNodeType().getTypeId()) ;
+        List<Map<String, Object>> tempOrderProducts = nodeBindingService.list("tempOrderProduct", "tempOrderId_matching="+data.get("tempOrderId"));
+
+        for(Map<String, Object> tempOrderProduct : tempOrderProducts){
+            List<Map<String, Object>> list = new ArrayList<>();
+            List<Map<String, Object>> coupons = getApplicableCoupons(nodeBindingInfo, data.get("memberNo").toString(), tempOrderProduct.get("productId").toString());
+            for(Map<String, Object> coupon : coupons){
+                Node node = nodeService.getNode("couponType", coupon.get("couponTypeId").toString());
+                Integer discountPrice = getDiscountPrice(node, tempOrderProduct.get("orderPrice"));
+                if(discountPrice > 0){
+                    coupon.put("discountPrice", discountPrice);
+                    list.add(coupon);
+                }
+            }
+
+            tempOrderProduct.put("applicableCoupons", list);
+
+        }
+
+        Map<String, Object> object = new HashMap<>();
+        object.put("items", tempOrderProducts);
+
+        context.setResult(object);
+
+        return context;
+    }
+
+    public Integer getDiscountPrice(Node couponType, Object orderPrice){
+        Integer result = 1000;
+        if("discountRate".equals(couponType.getValue("benefitsType"))){
+//            IF((15000 / 100 * y.benefitsPrice) > y.maxDiscountPrice, y.maxDiscountPrice,(15000 / 100 * y.benefitsPrice))
+
+        }else if("discountPrice".equals(couponType.getValue("benefitsType"))){
+//            IF(y.minPurchasePrice < 1500, y.benefitsPrice, 0)
+
+
+        }else if("freeDelivery".equals(couponType.getValue("benefitsType"))){
+
+        }
+
+        return result;
+    }
+
+    public List<Map<String, Object>> getApplicableCoupons(NodeBindingInfo nodeBindingInfo, String memberNo, String productId){
+        List<Map<String, Object>> list = new ArrayList<>();
+        String query = "SELECT a.*, c.productId as useableProductId\n" +
+                "FROM coupon a, coupontypetoproductmap c\n" +
+                "WHERE a.memberNo = ? AND c.productId = ? \n" +
+                "      AND a.couponTypeId = c.couponTypeId\n" +
+                "      AND a.siteType != IF(IFNULL('university', 'company') = 'university', 'company', 'university')\n" +
+                "UNION ALL\n" +
+                "SELECT a.*, c.productId as useableProductId\n" +
+                "FROM coupon a\n" +
+                "  , (\n" +
+                "      SELECT couponTypeId,productId\n" +
+                "      FROM couponTypeToCategoryMap c, producttocategorymap p\n" +
+                "      WHERE productId = ? \n" +
+                "            AND p.categoryId = c.categoryId\n" +
+                "      GROUP BY couponTypeId\n" +
+                "    ) c\n" +
+                "WHERE a.memberNo = ? \n" +
+                "      AND a.couponTypeId = c.couponTypeId";
+        list = nodeBindingInfo.getJdbcTemplate().queryForList(query, memberNo, productId, productId, memberNo);
+
+        return list;
+    }
 }
