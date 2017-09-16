8042b532c41446177aaecccfb534dc8618c22139
jaeho <pm1200@i-on.net>
2017-09-16 17:24:04 +0900

    query 수정

--- src/main/java/net/ion/ice/core/context/QueryContext.java
+++ src/main/java/net/ion/ice/core/context/QueryContext.java
@@ -109,7 +109,7 @@ public class QueryContext extends ReadContext {
 
     public void makeSearchFields(Map<String, Object> _config) {
         if(_config == null) return ;
-        String searchFieldsStr = (String) _config.get("searchFields");
+        String searchFieldsStr = (String)  ContextUtils.getValue(_config.get("searchFields"), data);
         if(org.apache.commons.lang3.StringUtils.isEmpty(searchFieldsStr)) {
             return ;
         }
