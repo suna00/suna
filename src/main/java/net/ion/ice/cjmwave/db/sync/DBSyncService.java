f4d8ba63a7ec424ff2a4828c039a2a47b87efc5a
juneyoungoh <jyoh@i-on.net>
2017-09-19 19:49:08 +0900

    이것저것 바꿈 - 특히 mnet 부분 마이그레이션 추가 (서브 데이터 처리하는 방식으로 in 절 적용)

--- src/main/java/net/ion/ice/cjmwave/db/sync/DBSyncService.java
+++ src/main/java/net/ion/ice/cjmwave/db/sync/DBSyncService.java
@@ -6,18 +6,19 @@ import net.ion.ice.cjmwave.external.utils.MigrationUtils;
 import net.ion.ice.core.data.DBService;
 import net.ion.ice.core.node.Node;
 import net.ion.ice.core.node.NodeService;
+import net.ion.ice.core.node.NodeType;
 import net.ion.ice.core.node.NodeUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by juneyoungoh on 2017. 9. 5..
@@ -38,6 +39,18 @@ public class DBSyncService {
 
     @Autowired
     DBService dbService;
+
+    private JdbcTemplate ice2Template;
+
+    @PostConstruct
+    public void init(){
+        try{
+            ice2Template = dbService.getJdbcTemplate("cjDb");
+        } catch (Exception e) {
+            logger.error("Could not initialize JdbcTemplate");
+        }
+    }
+
 
     /*
     * executeId 로 dbSyncProcess 노드를 뒤져서
@@ -80,6 +93,47 @@ public class DBSyncService {
             altered.add(fit);
         }
         return altered;
+    }
+
+    private List<String> executeSingleTaskAndRecord (Node executionNode, List<Map<String, Object>> queryRs, String mig_target, String mig_type) throws Exception {
+        List<String> successIds = new ArrayList<>();
+        int successCnt = 0;
+        int skippedCnt = 0;
+        Date startTime = new Date();
+        String executeId = executionNode.getId();
+        String targetNodeType = String.valueOf(executionNode.get("targetNodeType"));
+        String failPolicy = String.valueOf(executionNode.get("onFail")).trim().toUpperCase();
+        failPolicy = (!"NULL".equals(failPolicy) && "STOP".equals(failPolicy)) ? "STOP" : "SKIP";
+
+        List<Node> mapperInfoList = NodeUtils.getNodeList(MAPPER_TID, "executeId_matching=" + executeId);
+        Map<String, String> mapperStore = NodeMappingUtils.extractPropertyColumnMap(mapperInfoList);
+
+        for (Map qMap : queryRs) {
+            int rs = 0;
+            Map<String, Object> fit = NodeMappingUtils.mapData(targetNodeType, qMap, mapperStore);
+            logger.info("CREATE MIGRATION NODE :: " + String.valueOf(fit));
+            try{
+                Node finished = nodeService.saveNodeWithException(fit);
+                successIds.add(finished.getId());
+                successCnt ++;
+                rs = 1;
+            } catch (Exception e) {
+                // 실패한다면 실패 기록을 DB 에 저장한다.
+                logger.error("Recording exception :: ", e);
+                if(failPolicy.equals("STOP")){
+                    break;
+                } else {
+                    skippedCnt++;
+                }
+            }
+            MigrationUtils.recordSingleDate(ice2Template, targetNodeType, String.valueOf(fit), rs); // 레코드에 대한 결과
+        }
+
+        long jobTaken = (new Date().getTime() - startTime.getTime());
+        MigrationUtils.printReport(startTime, executeId, failPolicy, successCnt, skippedCnt);
+        MigrationUtils.recordResult(ice2Template, mig_target, mig_type, null, null
+                ,targetNodeType, successCnt, skippedCnt, jobTaken, startTime);
+        return  successIds;
     }
 
 
@@ -167,6 +221,56 @@ public class DBSyncService {
     public void executeForNewData (String mig_target, String executeId) throws Exception {
         // last Execution 시간은 MIG_DATA_HISTORY 에서 찾을 수 있음
         // MSSQL 펑션이 있다고 생각하고 파라미터로 마지막 날짜를 던져서 노드 생성하면 됨
+        String queryForLastExecution =
+                "SELECT execution_date as lastUpdated "
+                + "FROM MIG_HISTORY "
+                + "WHERE mig_target = ? AND target_node = ? AND mig_type='SCHEDULE' "
+                + "ORDER BY execution_date DESC LIMIT 1";
 
+        Node executionNode = nodeService.getNode(PROCESS_TID, executeId);
+        String targetNodeType = String.valueOf(executionNode.get("targetNodeType"));
+        String query = String.valueOf(executionNode.get("query"));
+
+        Map<String, Object> lastExecutionRs = null;
+        try {
+            lastExecutionRs = ice2Template.queryForMap(queryForLastExecution, mig_target, targetNodeType);
+        } catch (EmptyResultDataAccessException erda) {
+            logger.info("No data found");
+            lastExecutionRs = new HashMap<String, Object>();
+            lastExecutionRs.put("lastUpdated", new Date());
+        }
+
+        // 그래봤자 ID 속성이 뭔지 모르잖아
+        List<String> successIds = new ArrayList<>();
+        Map<String, Object> jdbcParam = SyntaxUtils.parse(query, lastExecutionRs);
+        String jdbcQuery = String.valueOf(jdbcParam.get("query"));
+        Object[] params = (Object[]) jdbcParam.get("params");
+
+        // executeId 실행하고 결과 처리
+        List<Map<String, Object>> targets2Update =
+                ice2Template.queryForList(jdbcQuery, params);
+
+
+        successIds = executeSingleTaskAndRecord(executionNode, targets2Update, "MNET", "SCHEDULE");
+
+        // successIds 로 다국어 노드 업데이트 - Naming rule 에 따라 움직이기
+        Node multiLanguageExecutionNode = nodeService.getNode(PROCESS_TID, executeId + "Multi");
+        if(null != multiLanguageExecutionNode) {
+            // 다국어 실행
+
+            /*
+            * 09 19
+            * 수정 *절대* 필요!!!!
+            * */
+            Map<String, Object> multiParamMap = new HashMap<>();
+            String strIds = StringUtils.join(successIds, ",").trim();
+            if(strIds.length() > 0) {
+                multiParamMap.put("ids", strIds);
+                String multiQuery = String.valueOf(multiLanguageExecutionNode.get("query"));
+                List<Map<String, Object>> multiLanguageTargets =
+                        ice2Template.queryForList(multiQuery, multiParamMap);
+                executeSingleTaskAndRecord(multiLanguageExecutionNode, multiLanguageTargets, "MNET", "SCHEDULE");
+            }
+        }
     }
 }
