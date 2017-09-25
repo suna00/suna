955440eae9f6f887e09052c1750dcc45e6ce4e6f
yeon <yeon0153@i-on.net>
2017-09-25 11:39:53 +0900

    회원정보 수정

--- src/main/java/net/ion/ice/service/MemberService.java
+++ src/main/java/net/ion/ice/service/MemberService.java
@@ -8,6 +8,8 @@ import net.ion.ice.core.node.NodeService;
 import net.ion.ice.core.node.NodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -26,69 +28,88 @@ public class MemberService {
     private CommonService commonService;
     private EmailService emailService;
 
+    public ExecuteContext saveMemberInfo(ExecuteContext context){
+        Map<String, Object> contextData = new LinkedHashMap<>(context.getData());
+        Map<String, Object> resultObject = new HashMap<>();
+        Map<String, Object> item = new HashMap<>();
+
+        Node node;
+
+        if(contextData.get("memberNo") == null){
+            if(contextData.containsKey("password")){
+                String password = SHA256(contextData.get("password").toString());
+                contextData.put("password", password);
+            }
+            node = (Node)nodeService.executeNode(contextData, "member", CREATE);
+
+        } else {
+            if(contextData.containsKey("password")){
+                String password = SHA256(contextData.get("password").toString());
+                contextData.put("password", password);
+            }
+            node = (Node)nodeService.executeNode(contextData, "member", UPDATE);
+        }
+
+        item.put("memberNo", node.get("memberNo"));
+        item.put("email", node.get("email"));
+        item.put("name", node.get("name"));
+
+        resultObject.put("item", item);
+        context.setResult(resultObject);
+
+        return context;
+    }
+
     public ExecuteContext authenticationSendEmail(ExecuteContext context){
         Map<String, Object> data = new LinkedHashMap<>(context.getData());
 
-        String[] params = { "email","siteType","acceptTermsYn","receiveMarketingEmailAgreeYn","receiveMarketingSMSAgreeYn" };
+        String[] params = { "emailCertificationType" };
         if (commonService.requiredParams(context, data, params)) return context;
 
-        String email = data.get("email").toString();
-        String siteType = data.get("siteType").toString();
-        String acceptTermsYn = data.get("acceptTermsYn").toString();
-        String receiveMarketingEmailAgreeYn = data.get("receiveMarketingEmailAgreeYn").toString();
-        String receiveMarketingSMSAgreeYn = data.get("receiveMarketingSMSAgreeYn").toString();
-
         NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("member");
-        String count = nodeBindingInfo.getJdbcTemplate().queryForList(" select count(memberNo) as count from member where email=? ", email).get(0).get("count").toString();
+        String emailCertificationType = data.get("emailCertificationType").toString();
 
-        if("0".equals(count)){
-            String certCode = getCertCode("이메일 인증요청", "join", null, email, "request");
-            String linkUrl = "/signUp/stepTwo?certCode="+certCode+"&siteType="+siteType+"&acceptTermsYn="+acceptTermsYn+"&receiveMarketingEmailAgreeYn="+receiveMarketingEmailAgreeYn+"&receiveMarketingSMSAgreeYn="+receiveMarketingSMSAgreeYn;
-            Map<String, String> html = memberHtml("본인인증", linkUrl);
+        // 회원가입 : join
+        if("join".equals(emailCertificationType)){
+            String email = data.get("email").toString();
+            String count = nodeBindingInfo.getJdbcTemplate().queryForList(" select count(memberNo) as count from member where email=? ", email).get(0).get("count").toString();
 
-            emailService.sendEmailHtml(email, html.get("title"), html.get("contents"));
-        } else {
-            commonService.setErrorMessage(context, "U0001");
+            if("0".equals(count)){
+                sendEmail(emailCertificationType, email, data);
+            } else {
+                commonService.setErrorMessage(context, "U0001"); return context;
+            }
+        }
+
+        // 비밀번호 : password, 휴면회원해제:sleep
+        if("password".equals(emailCertificationType) || "sleep".equals(emailCertificationType)){
+            List<Map<String,Object>> memberList = nodeBindingInfo.getJdbcTemplate().queryForList(" select * from member where name=? and userId=? ", data.get("name").toString(), data.get("userId").toString());
+
+            if(0 < memberList.size()){
+                Map<String, Object> member = memberList.get(0);
+                sendEmail(emailCertificationType, member.get("email").toString(), member);
+
+                Map<String, Object> resultObject = new HashMap<>();
+                resultObject.put("email", member.get("email"));
+                context.setResult(resultObject);
+            } else {
+                commonService.setErrorMessage(context, "U0004"); return context;
+            }
         }
 
         return context;
     }
 
-    public ExecuteContext searchPassword(ExecuteContext context){
+    public ExecuteContext authenticationSendSms(ExecuteContext context){
         Map<String, Object> data = new LinkedHashMap<>(context.getData());
 
-        String[] params = { "searchType","name","userId" };
+        String[] params = { "smsCertificationType" };
         if (commonService.requiredParams(context, data, params)) return context;
 
-        String searchType = data.get("searchType").toString();
-        String name = data.get("name").toString();
-        String userId = data.get("userId").toString();
+        String smsCertificationType = data.get("smsCertificationType").toString(); // 아이디 : id, 패스워드 : password, 휴면회원해제 : sleep
+        String cellphone = data.get("cellphone").toString();
 
-        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("member");
-        List<Map<String,Object>> memberList = nodeBindingInfo.getJdbcTemplate().queryForList(" select * from member where name=? and userId=? ", name, userId);
-
-        if(0 < memberList.size()){
-            Map<String, Object> member = memberList.get(0);
-            String memberNo = member.get("memberNo").toString();
-            String email = member.get("email").toString();
-            String cellPhone = member.get("cellPhone").toString();
-
-            if("email".equals(searchType)){
-                String certCode = getCertCode("이메일 인증요청", "password", memberNo, email, "request");
-                String linkUrl = "/signIn/changePassword?certCode="+certCode;
-                Map<String, String> html = memberHtml("비밀번호변경", linkUrl);
-
-                emailService.sendEmailHtml(member.get("email").toString(), html.get("title"), html.get("contents"));
-
-                Map<String, Object> resultObject = new HashMap<>();
-                resultObject.put("email", member.get("email").toString());
-                context.setResult(resultObject);
-            } else {
-                String cellPhoneData = data.get("cellPhoneData").toString();
-            }
-        } else {
-            commonService.setErrorMessage(context, "U0004");
-        }
+        sendSms(smsCertificationType, cellphone, data);
 
         return context;
     }
@@ -133,6 +154,168 @@ public class MemberService {
         }
 
         return context;
+    }
+
+    public ExecuteContext authenticationCertSms(ExecuteContext context){
+        Map<String, Object> data = new LinkedHashMap<>(context.getData());
+
+        String[] params = { "certCode" };
+        if (commonService.requiredParams(context, data, params)) return context;
+
+        String certCode = data.get("certCode").toString();
+
+        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("smsCertification");
+        String query = " SELECT smsCertificationId, memberNo, certCode, certStatus, certRequestDate, date_add(certRequestDate, INTERVAL +3 MINUTE) AS certExpireDate, (certStatus = 'request' AND date_add(certRequestDate, INTERVAL +3 MINUTE) > now() AND certCode = ?) AS available FROM smscertification WHERE certCode = ? limit 1";
+        List<Map<String, Object>> list = nodeBindingInfo.getJdbcTemplate().queryForList(query, certCode, certCode);
+
+        if(0 < list.size()){
+            Map<String,Object> map = list.get(0);
+            String available = map.get("available").toString();
+
+            if("1".equals(available)){
+                Map<String, Object> smsCertificationData = new HashMap<>();
+                smsCertificationData.put("smsCertificationId", map.get("smsCertificationId"));
+                smsCertificationData.put("certStatus", "success");
+                smsCertificationData.put("certSuccessDate", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+
+                nodeService.executeNode(smsCertificationData, "emailCertification", UPDATE);
+
+                Map<String, Object> resultObject = new HashMap<>();
+                Map<String, Object> item = new HashMap<>();
+
+                item.put("memberNo", map.get("memberNo"));
+                resultObject.put("item", item);
+
+                context.setResult(resultObject);
+            } else{
+                commonService.setErrorMessage(context, "U0003");
+            }
+        } else {
+            commonService.setErrorMessage(context, "U0002");
+        }
+
+        return context;
+    }
+
+    public String SHA256(String password){
+        String encodingPassword;
+
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            encodingPassword = hexString.toString();
+            System.out.println("encodingPassword : " + encodingPassword);
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+
+        return  password; //encodingPassword;
+    }
+
+    public void sendEmail(String emailCertificationType, String email, Map<String, Object> data){
+        Map<String, String> html = new HashMap<>();
+
+        // 회원가입 : join
+        if("join".equals(emailCertificationType)){
+            String certCode = getEmailCertCode("이메일 인증요청", emailCertificationType, null, email, "request");
+            String linkUrl = "/signUp/stepTwo?certCode="+certCode+"&siteType="+data.get("siteType")+"&acceptTermsYn="+data.get("acceptTermsYn")+"&receiveMarketingEmailAgreeYn="+data.get("receiveMarketingEmailAgreeYn")+"&receiveMarketingSMSAgreeYn="+data.get("receiveMarketingSMSAgreeYn");
+            html = setHtml("본인인증", linkUrl);
+        }
+
+        // 비밀번호 : password
+        if("password".equals(emailCertificationType)){
+            String certCode = getEmailCertCode("이메일 인증요청", emailCertificationType, data.get("memberNo").toString(), email, "request");
+            String linkUrl = "/signIn/changePassword?certCode="+certCode;
+            html = setHtml("비밀번호변경", linkUrl);
+        }
+
+        // 휴면회원:sleep
+        if("sleep".equals(emailCertificationType)){
+            String certCode = getEmailCertCode("이메일 인증요청", emailCertificationType, data.get("memberNo").toString(), email, "request");
+            String linkUrl = "/signIn/changePassword?certCode="+certCode;
+            html = setHtml("휴면해제이메일인증", linkUrl);
+        }
+
+        emailService.sendEmailHtml(email, html.get("title"), html.get("contents"));
+    }
+
+    public void sendSms(String smsCertificationType, String cellphone, Map<String, Object> data){
+        // 아이디 : id, 패스워드 : password, 휴면회원 : sleepMember
+
+        String certCdoe = getSmsCertCode("SMS 인증요청", smsCertificationType, data.get("memberNo").toString(), cellphone, "request");
+        String Message = "인증번호["+certCdoe+"]";
+
+        // 문자 전송
+    }
+
+    public String getEmailCertCode(String name, String emailCertificationType, String memberNo, String email, String certStatus){
+        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("emailCertification");
+        String certCode = nodeBindingInfo.getJdbcTemplate().queryForList(" select to_base64(concat('ygoon!@#SHOP ',now())) as certCode ").get(0).get("certCode").toString();
+
+        Map<String, Object> emailCertificationData = new HashMap<>();
+        emailCertificationData.put("name",name);
+        emailCertificationData.put("emailCertificationType", emailCertificationType);
+        emailCertificationData.put("memberNo", memberNo);
+        emailCertificationData.put("email", email);
+        emailCertificationData.put("certCode", certCode);
+        emailCertificationData.put("certStatus", certStatus);
+        emailCertificationData.put("certRequestDate", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+        nodeService.executeNode(emailCertificationData, "emailCertification", CREATE);
+
+        return certCode;
+    }
+
+    public String getSmsCertCode(String name, String smsCertificationType, String memberNo, String cellphone, String certStatus){
+        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("smsCertification");
+        String certCode = nodeBindingInfo.getJdbcTemplate().queryForList(" select FLOOR(1000 + (RAND()*8999)) as certCode ").get(0).get("certCode").toString();
+
+        Map<String, Object> smsCertificationData = new HashMap<>();
+        smsCertificationData.put("name",name);
+        smsCertificationData.put("smsCertificationType", smsCertificationType);
+        smsCertificationData.put("memberNo", memberNo);
+        smsCertificationData.put("cellphone", cellphone);
+        smsCertificationData.put("certCode", certCode);
+        smsCertificationData.put("certStatus", certStatus);
+        smsCertificationData.put("certRequestDate", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+        nodeService.executeNode(smsCertificationData, "smsCertification", CREATE);
+
+        return certCode;
+    }
+
+    public Map<String, String> setHtml(String templateName, String linkUrl){
+        Map<String, String> setHtmlMap = new HashMap<>();
+
+        String contextPath = commonService.replaceUrl();
+        LocalDateTime date = LocalDateTime.now().plusHours(1);
+
+        String title = "";
+        String contents = "";
+        String link = "http://localhost:3090"+linkUrl; // http://125.131.88.206:3090
+
+        List<Map<String, Object>> emailTemplateList = nodeBindingService.list("emailTemplate", "name_in=".concat(templateName));
+
+        if(emailTemplateList.size() > 0){
+            title = emailTemplateList.get(0).get("title").toString();
+            contents = emailTemplateList.get(0).get("contents").toString();
+
+            contents = contents.replaceAll("src=\"http://localhost/assets/images","src="+contextPath+"/image");
+            contents = contents.replaceAll("href=\"#\"", "href="+link);
+            contents = contents.replaceAll("yyyy-MM-dd", date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))); // yyyy-MM-dd
+            contents = contents.replaceAll("HH:mm:ss", date.format(DateTimeFormatter.ofPattern("HH:mm:ss"))); // HH:mm:ss
+        }
+
+        setHtmlMap.put("title", title);
+        setHtmlMap.put("contents", contents);
+
+        return setHtmlMap;
     }
 
     public ExecuteContext leaveMembership(ExecuteContext context){
@@ -200,50 +383,5 @@ public class MemberService {
             ranPw += pwCollection[selectRandomPw];
         }
         return ranPw;
-    }
-
-    public String getCertCode(String name, String emailCertificationType, String memberNo, String email, String certStatus){
-        NodeBindingInfo nodeBindingInfo = NodeUtils.getNodeBindingInfo("emailCertification");
-        String certCode = nodeBindingInfo.getJdbcTemplate().queryForList(" select to_base64(concat('ygoon!@#SHOP ',now())) as certCode ").get(0).get("certCode").toString();
-
-        Map<String, Object> emailCertificationData = new HashMap<>();
-        emailCertificationData.put("name",name);
-        emailCertificationData.put("emailCertificationType", emailCertificationType);
-        emailCertificationData.put("memberNo", memberNo);
-        emailCertificationData.put("email", email);
-        emailCertificationData.put("certCode", certCode);
-        emailCertificationData.put("certStatus", certStatus);
-        emailCertificationData.put("certRequestDate", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
-        nodeService.executeNode(emailCertificationData, "emailCertification", CREATE);
-
-        return certCode;
-    }
-
-    public Map<String, String> memberHtml(String templateName, String linkUrl){
-        Map<String, String> html = new HashMap<>();
-
-        String contextPath = commonService.replaceUrl();
-        LocalDateTime date = LocalDateTime.now().plusHours(1);
-
-        String title = "";
-        String contents = "";
-        String link = "http://localhost:3090"+linkUrl;
-
-        List<Map<String, Object>> emailTemplateList = nodeBindingService.list("emailTemplate", "name_in=".concat(templateName));
-
-        if(emailTemplateList.size() > 0){
-            title = emailTemplateList.get(0).get("title").toString();
-            contents = emailTemplateList.get(0).get("contents").toString();
-
-            contents = contents.replaceAll("src=\"http://localhost/assets/images","src="+contextPath+"/image");
-            contents = contents.replaceAll("href=\"#\"", "href="+link);
-            contents = contents.replaceAll("yyyy-MM-dd", date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))); // yyyy-MM-dd
-            contents = contents.replaceAll("HH:mm:ss", date.format(DateTimeFormatter.ofPattern("HH:mm:ss"))); // HH:mm:ss
-        }
-
-        html.put("title", title);
-        html.put("contents", contents);
-
-        return html;
     }
 }
