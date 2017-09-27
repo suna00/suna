2c6f7f9ca41272993aaa8415e4c359a16ddc370f
jjd <jjd929@i-on.net>
2017-09-27 23:20:56 +0900

    Sub 저장할때 부모 데이터들 안늠. 기본값은 UI 에서 처리 가능.

--- src/main/java/net/ion/ice/core/context/ExecuteContext.java
+++ src/main/java/net/ion/ice/core/context/ExecuteContext.java
@@ -251,8 +251,8 @@ public class ExecuteContext extends ReadContext{
             if(value instanceof List && (this.nodeType.getPropertyType(key) == null || !this.nodeType.getPropertyType(key).isList()) && NodeUtils.getNodeType(key) != null){
                 for(Map<String, Object> subData : (List<Map<String, Object>>)value){
                     Map<String, Object> _data = new HashMap<>() ;
-                    _data.putAll(data);
-                    _data.remove(key) ;
+//                    _data.putAll(data);
+//                    _data.remove(key) ;
                     _data.putAll(subData);
                     for(String _key : subData.keySet()){
                         Object _val = subData.get(_key) ;
