9d0e3aa11e43dd7b16375aef1efad62627afbeda
leehh <leehh0713>
2017-10-12 20:20:08 +0900

    [leehh][CJMwave]뉴스 api에 언어코드가 eng,zho-cn,zho-tw외에는 무조건 eng로 조회되도록 수정, 회원단말정보 저장시 앱정보 생성되도록 추가,푸쉬메세지 적용

--- src/main/java/net/ion/ice/cjmwave/mbrSttusHst/MbrSttusHstService.java
+++ src/main/java/net/ion/ice/cjmwave/mbrSttusHst/MbrSttusHstService.java
@@ -5,6 +5,7 @@ package net.ion.ice.cjmwave.mbrSttusHst;
  */
 
 import java.util.List;
+
 import net.ion.ice.core.context.ExecuteContext;
 import net.ion.ice.core.event.EventService;
 import net.ion.ice.core.node.Node;
@@ -18,51 +19,61 @@ import java.util.Map;
 
 @Service("mbrSttusHstService")
 public class MbrSttusHstService {
+
+    public static final String MBR_STTUS_CD = "mbrSttusCd";
+    public static final String STTUS_CHG_SBST = "sttusChgSbst";
+    public static final String MBR_ID = "mbrId";
+    public static final String MBR_STTUS_HST = "mbrSttusHst";
+
     public void sttusHstCreate(ExecuteContext context) {
         try {
             Node mbrNode = context.getNode();
             Node mbrExistNode = context.getExistNode();
             List<String> changeProperties = context.getChangedProperties();
             Map<String, Object> data = context.getData();
-            //context 에 있는 existNode와 상태코드를 비교해서 변경된 경우에만 이력을 쌓아야함
+
             Map<String, Object> createData = new LinkedHashMap<>();
 
-            if (StringUtils.isEmpty(mbrNode.getId()) || StringUtils.isEmpty(mbrNode.getStringValue("mbrSttusCd"))) {
+            if (StringUtils.isEmpty(mbrNode.getId()) || StringUtils.isEmpty(mbrNode.getStringValue(MBR_STTUS_CD))) {
                 //skip
                 return;
             }
-
-            if(changeProperties.isEmpty() || changeProperties.size() == 0){
-                //skip
-                return;
-            }
-
-            Boolean sttusChgYn = false;
-            for(String pid :changeProperties){
-                if("mbrSttusCd".equals(pid)){
-                    sttusChgYn = true;
-                }
-            }
-
-            //create
-            String mbrSttusCd = mbrNode.getStringValue("mbrSttusCd");
-            String existSttusCd = mbrExistNode.getStringValue("mbrSttusCd");
-            if(!sttusChgYn || mbrSttusCd.equals(existSttusCd)){
-                //skip
-                return;
-            }
+            String mbrSttusCd = mbrNode.getStringValue(MBR_STTUS_CD);
             if (StringUtils.contains(mbrSttusCd, "mbrSttusCd>")) {
                 mbrSttusCd = StringUtils.replace(mbrSttusCd, "mbrSttusCd>", "");
             }
             String sttusChgSbst = "";
-            if (data.get("sttusChgSbst") != null && !StringUtils.isEmpty(data.get("sttusChgSbst").toString())) {
-                sttusChgSbst = data.get("sttusChgSbst").toString();
+            if (data.get(STTUS_CHG_SBST) != null && !StringUtils.isEmpty(data.get(STTUS_CHG_SBST).toString())) {
+                sttusChgSbst = data.get(STTUS_CHG_SBST).toString();
             }
-            createData.put("mbrId", mbrNode.getId());
-            createData.put("mbrSttusCd", mbrSttusCd);
-            createData.put("sttusChgSbst", sttusChgSbst);
 
-            NodeUtils.getNodeService().executeNode(createData, "mbrSttusHst", EventService.CREATE);
+            if (mbrExistNode != null) {//mbrUpdate
+                //context 에 있는 existNode와 상태코드를 비교해서 변경된 경우에만 이력을 쌓아야함
+                if (changeProperties == null || changeProperties.isEmpty() || changeProperties.size() == 0) {
+                    //skip
+                    return;
+                }
+
+                Boolean sttusChgYn = false;
+                for (String pid : changeProperties) {
+                    if (MBR_STTUS_CD.equals(pid)) {
+                        sttusChgYn = true;
+                    }
+                }
+
+                //create
+                String existSttusCd = mbrExistNode.getStringValue(MBR_STTUS_CD);
+                if (!sttusChgYn || mbrSttusCd.equals(existSttusCd)) {
+                    //skip
+                    return;
+                }
+
+            }
+            createData.put(MBR_ID, mbrNode.getId());
+            createData.put(MBR_STTUS_CD, mbrSttusCd);
+            createData.put(STTUS_CHG_SBST, sttusChgSbst);
+
+            NodeUtils.getNodeService().executeNode(createData, MBR_STTUS_HST, EventService.CREATE);
         } catch (Exception e) {
             e.printStackTrace();
         }
