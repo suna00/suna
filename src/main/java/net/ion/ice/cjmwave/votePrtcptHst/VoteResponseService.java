fce76b2e0816219e0d42097d5431c86103cc93b2
leehh <leehh0713>
2017-10-15 22:06:19 +0900

    [leehh][CJMwave]IfMWv001,108 response 수정

--- src/main/java/net/ion/ice/cjmwave/votePrtcptHst/VoteResponseService.java
+++ src/main/java/net/ion/ice/cjmwave/votePrtcptHst/VoteResponseService.java
@@ -4,12 +4,19 @@ package net.ion.ice.cjmwave.votePrtcptHst;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+import net.ion.ice.ApplicationContextManager;
 import net.ion.ice.core.api.ApiException;
 import net.ion.ice.core.context.ContextUtils;
 import net.ion.ice.core.context.ExecuteContext;
+import net.ion.ice.core.context.QueryContext;
+import net.ion.ice.core.infinispan.InfinispanRepositoryService;
 import net.ion.ice.core.node.Node;
 import net.ion.ice.core.node.NodeService;
+import net.ion.ice.core.node.NodeType;
 import net.ion.ice.core.node.NodeUtils;
+import net.ion.ice.core.query.QueryResult;
+import net.ion.ice.core.query.QueryTerm;
+import net.ion.ice.core.query.QueryUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.commons.lang3.time.DateUtils;
@@ -27,6 +34,7 @@ public class VoteResponseService {
     private NodeService nodeService;
 
     private JdbcTemplate jdbcTemplate;
+    static InfinispanRepositoryService infinispanRepositoryService;
 
     public void voteStatus(ExecuteContext context) {
         Map<String, Object> data = context.getData();
@@ -71,52 +79,72 @@ public class VoteResponseService {
         if (jdbcTemplate == null) {
             jdbcTemplate = NodeUtils.getNodeBindingService().getNodeBindingInfo(VOTE_BAS_INFO).getJdbcTemplate();
         }
+        if (infinispanRepositoryService == null) {
+            infinispanRepositoryService = ApplicationContextManager.getBean(InfinispanRepositoryService.class);
+        }
 
         String term = "voteFormlCd_matching={{:voteFormlCd}}&showLoCd_matching={{:showLoCd}}&pstngStDt_below={{:conditionSysdate(progYn,true)}}"
                 + "&pstngFnsDt_above={{:conditionSysdate(progYn,true)}}&pstngFnsDt_under={{:conditionSysdate(progYn,false)}}&showYn_matching=true&evVoteYn_matching=false"
                 + "&voteNm_{{:langCd}}_wildcardShould={{:searchKeyword}}*&voteDesc_{{:langCd}}_wildcardShould={{:searchKeyword}}*"
                 + "&findKywrd_matchingShould={{:searchKeyword}}&sorting={{:sorting}}&page={{:page}}&pageSize={{:pageSize}}&langCd_matching={{:langCd}}";
-
-        Map<String, Object> data = context.getData();
-        if (data.get("pageSize")==null || Integer.parseInt(data.get("pageSize").toString())<=0) {
-            data.put("pageSize", 10);
-        }
-        if (data.get("page")==null || Integer.parseInt(data.get("page").toString())<=0) {
-            data.put("page", 1);
-        }
-
-        context.setIncludeReferenced(true);
-
-        List<Node> voteInfoList = nodeService.getNodeList(VOTE_BAS_INFO, (String) ContextUtils.getValue(term, data));
-
-        for (Node voteBasInfo : voteInfoList) {
-            // TODO - voteSeq_referenceJoin 처리관련 문의 요청 필요
-            String voteItemTerms = "voteSeq_matching=" + voteBasInfo.getId() +
-                    "&langCd_matching=" + data.get("langCd");
-
-            List<Node> refdItemList = nodeService.getNodeList(VOTE_ITEM_INFO, voteItemTerms);
-            for (Node refdItem : refdItemList) {
-                refdItem.toDisplay(context);
+        try {
+            Map<String, Object> data = context.getData();
+            if (data.get("pageSize") == null || Integer.parseInt(data.get("pageSize").toString()) <= 0) {
+                data.put("pageSize", 10);
+            }
+            if (data.get("page") == null || Integer.parseInt(data.get("page").toString()) <= 0) {
+                data.put("page", 1);
             }
 
-            List<Node> refdSeriesItemList = nodeService.getNodeList(SERS_VOTE_ITEM_INFO, voteItemTerms);
-            for (Node refdSeriesItem : refdSeriesItemList) {
-                refdSeriesItem.toDisplay(context);
+            context.setIncludeReferenced(true);
+
+            NodeType voteBasInfoNodeType = NodeUtils.getNodeType(VOTE_BAS_INFO);
+            //List<Node> voteInfoList = nodeService.getNodeList(VOTE_BAS_INFO, (String) ContextUtils.getValue(term, data));
+
+            Object values = ContextUtils.getValue(term, data);
+            //System.out.println("#####values :" + values);
+            QueryContext queryContext = QueryContext.createQueryContextFromText((String) values, voteBasInfoNodeType, null);
+            List<Node> voteInfoList = infinispanRepositoryService.getSubQueryNodes(voteBasInfoNodeType.getTypeId(), queryContext);
+
+            QueryResult queryResult = new QueryResult();
+            queryResult.put("totalCount", queryContext.getResultSize());
+            queryResult.put("resultCount", voteInfoList.size());
+            queryResult.put("pageSize", queryContext.getPageSize());
+            queryResult.put("pageCount", queryContext.getResultSize() / queryContext.getPageSize() + (queryContext.getResultSize() % queryContext.getPageSize() > 0 ? 1 : 0));
+            queryResult.put("currentPage", queryContext.getCurrentPage());
+
+            for (Node voteBasInfo : voteInfoList) {
+                // TODO - voteSeq_referenceJoin 처리관련 문의 요청 필요
+                String voteItemTerms = "voteSeq_matching=" + voteBasInfo.getId() +
+                        "&langCd_matching=" + data.get("langCd");
+
+                List<Node> refdItemList = nodeService.getNodeList(VOTE_ITEM_INFO, voteItemTerms);
+                for (Node refdItem : refdItemList) {
+                    refdItem.toDisplay(context);
+                }
+
+                List<Node> refdSeriesItemList = nodeService.getNodeList(SERS_VOTE_ITEM_INFO, voteItemTerms);
+                for (Node refdSeriesItem : refdSeriesItemList) {
+                    refdSeriesItem.toDisplay(context);
+                }
+
+                voteBasInfo.toDisplay(context);
+
+                voteBasInfo.put("refdItemList", refdItemList);
+                voteBasInfo.put("refdSeriesItemList", refdSeriesItemList);
+
+                // voteNum 계산
+                Integer voteNum = getVoteNum(voteBasInfo.getId());
+                voteBasInfo.put("voteNum", (voteNum == null) ? 0 : voteNum);
             }
 
-            voteBasInfo.toDisplay(context);
-
-            voteBasInfo.put("refdItemList", refdItemList);
-            voteBasInfo.put("refdSeriesItemList", refdSeriesItemList);
-
-            // voteNum 계산
-            Integer voteNum = getVoteNum(voteBasInfo.getId());
-            voteBasInfo.put("voteNum", (voteNum == null) ? 0 : voteNum);
+            //data.put("items", voteInfoList);
+            queryResult.put("items", voteInfoList);
+            context.setResult(queryResult);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        data.put("items", voteInfoList);
-
-        context.setResult(data);
     }
 
 
@@ -134,7 +162,7 @@ public class VoteResponseService {
         List<Node> voteInfoList = nodeService.getNodeList(VOTE_BAS_INFO, (String) ContextUtils.getValue(term, data));
 
         String mbrId = "";
-        if (data.get("snsTypeCd")!=null || data.get("snsKey")!=null) {
+        if (data.get("snsTypeCd") != null || data.get("snsKey") != null) {
             mbrId = data.get("snsTypeCd").toString() + ">" + data.get("snsKey").toString();
         }
 
@@ -180,7 +208,7 @@ public class VoteResponseService {
 
             // userVoteCnt
             Integer userVoteCnt = 0;
-            if (mbrId!=null && mbrId.length()>0) {
+            if (mbrId != null && mbrId.length() > 0) {
                 userVoteCnt = getUserVoteCnt(voteBasInfo.getId(), mbrId);
             }
             voteBasInfo.put("userVoteCnt", (userVoteCnt == null) ? 0 : userVoteCnt);
@@ -220,72 +248,95 @@ public class VoteResponseService {
         if (jdbcTemplate == null) {
             jdbcTemplate = NodeUtils.getNodeBindingService().getNodeBindingInfo(VOTE_BAS_INFO).getJdbcTemplate();
         }
+        if (infinispanRepositoryService == null) {
+            infinispanRepositoryService = ApplicationContextManager.getBean(InfinispanRepositoryService.class);
+        }
 
         String term = "voteFormlCd_matching=2&showLoCd_matching=4&showYn_matching=true&evVoteYn_matching=false"
                 + "&bradTelcsDt_above={{:concatStr(bradTelcsDt,000000)}}&bradTelcsDt_below={{:concatStr(bradTelcsDt,235959)}}&"
                 + "&sorting=voteSeq desc&limit=1";
+        try {
+            Map<String, Object> data = context.getData();
+            if (data.get("pageSize") == null || Integer.parseInt(data.get("pageSize").toString()) <= 0) {
+                data.put("pageSize", 10);
+            }
+            if (data.get("page") == null || Integer.parseInt(data.get("page").toString()) <= 0) {
+                data.put("page", 1);
+            }
 
-        Map<String, Object> data = context.getData();
-        List<Node> voteInfoList = nodeService.getNodeList(VOTE_BAS_INFO, (String) ContextUtils.getValue(term, data));
+            context.setIncludeReferenced(true);
 
-        String mbrId = "";
-        if (data.get("snsTypeCd")!=null || data.get("snsKey")!=null) {
-            mbrId = data.get("snsTypeCd").toString() + ">" + data.get("snsKey").toString();
+            NodeType voteBasInfoNodeType = NodeUtils.getNodeType(VOTE_BAS_INFO);
+            //List<Node> voteInfoList = nodeService.getNodeList(VOTE_BAS_INFO, (String) ContextUtils.getValue(term, data));
+
+            Object values = ContextUtils.getValue(term, data);
+            //System.out.println("#####values :" + values);
+            QueryContext queryContext = QueryContext.createQueryContextFromText((String) values, voteBasInfoNodeType, null);
+            List<Node> voteInfoList = infinispanRepositoryService.getSubQueryNodes(voteBasInfoNodeType.getTypeId(), queryContext);
+
+            QueryResult queryResult = new QueryResult();
+            queryResult.put("totalCount", queryContext.getResultSize());
+            queryResult.put("resultCount", voteInfoList.size());
+
+            String mbrId = "";
+            if (data.get("snsTypeCd") != null || data.get("snsKey") != null) {
+                mbrId = data.get("snsTypeCd").toString() + ">" + data.get("snsKey").toString();
+            }
+
+            Date now = new Date();
+            String connIpAdr = data.get("connIpAdr").toString();
+            String voteDate = DateFormatUtils.format(now, "yyyyMMdd");
+
+            // Checking Available IP with mbrId and voteSeq
+            String searchText = "setupTypeCd_matching=2&sorting=dclaSetupSeq desc&limit=1";
+            List<Node> dclaNodeList = nodeService.getNodeList("dclaSetupMng", searchText);
+            Node dclaNode = dclaNodeList.get(0);
+            Integer ipDclaCnt = dclaNode.getIntValue("setupBaseCnt");
+            Integer ipCnt = getIpCnt(connIpAdr, voteDate);
+
+            Integer ipAdrVoteCnt = ipDclaCnt - ipCnt;
+
+            for (Node voteBasInfo : voteInfoList) {
+                String voteItemTerms = "voteSeq_matching=" + voteBasInfo.getId() +
+                        "&langCd_matching=" + data.get("langCd");
+
+                List<Node> voteItemList = nodeService.getNodeList(VOTE_ITEM_INFO, voteItemTerms);
+                for (Node voteItem : voteItemList) {
+                    context.makeReferenceView("contsMetaId"); // referenceView 설정
+                    voteItem.toDisplay(context);
+                }
+
+                List<Node> sersVoteItemList = nodeService.getNodeList(SERS_VOTE_ITEM_INFO, voteItemTerms);
+                for (Node voteItem : sersVoteItemList) {
+                    context.makeReferenceView("sersItemVoteSeq"); // referenceView 설정
+                    context.setIncludeReferenced(true);
+                    voteItem.toDisplay(context);
+                }
+
+                voteBasInfo.toDisplay(context);
+
+                voteBasInfo.put("refdItemList", voteItemList);
+                voteBasInfo.put("refdSeriesItemList", sersVoteItemList);
+
+                // voteNum 계산
+                Integer voteNum = getVoteNum(voteBasInfo.getId());
+                voteBasInfo.put("voteNum", (voteNum == null) ? 0 : voteNum);
+
+                // userVoteCnt
+                Integer userVoteCnt = 0;
+                if (mbrId != null && mbrId.length() > 0) {
+                    userVoteCnt = getUserVoteCnt(voteBasInfo.getId(), mbrId);
+                }
+                voteBasInfo.put("userVoteCnt", (userVoteCnt == null) ? 0 : userVoteCnt);
+                voteBasInfo.put("userPvCnt", 0);
+                voteBasInfo.put("ipAdrVoteCnt", ipAdrVoteCnt);
+            }
+
+            queryResult.put("items", voteInfoList);
+            context.setResult(queryResult);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        Date now = new Date();
-        String connIpAdr = data.get("connIpAdr").toString();
-        String voteDate = DateFormatUtils.format(now, "yyyyMMdd");
-
-        // Checking Available IP with mbrId and voteSeq
-        String searchText = "setupTypeCd_matching=2&sorting=dclaSetupSeq desc&limit=1";
-        List<Node> dclaNodeList = nodeService.getNodeList("dclaSetupMng", searchText);
-        Node dclaNode = dclaNodeList.get(0);
-        Integer ipDclaCnt = dclaNode.getIntValue("setupBaseCnt");
-        Integer ipCnt = getIpCnt(connIpAdr, voteDate);
-
-        Integer ipAdrVoteCnt = ipDclaCnt - ipCnt;
-
-        for (Node voteBasInfo : voteInfoList) {
-
-            String voteItemTerms = "voteSeq_matching=" + voteBasInfo.getId() +
-                    "&langCd_matching=" + data.get("langCd");
-
-            List<Node> voteItemList = nodeService.getNodeList(VOTE_ITEM_INFO, voteItemTerms);
-            for (Node voteItem : voteItemList) {
-                context.makeReferenceView("contsMetaId"); // referenceView 설정
-                voteItem.toDisplay(context);
-            }
-
-            List<Node> sersVoteItemList = nodeService.getNodeList(SERS_VOTE_ITEM_INFO, voteItemTerms);
-            for (Node voteItem : sersVoteItemList) {
-                context.makeReferenceView("sersItemVoteSeq"); // referenceView 설정
-                context.setIncludeReferenced(true);
-                voteItem.toDisplay(context);
-            }
-
-            voteBasInfo.toDisplay(context);
-
-            voteBasInfo.put("refdItemList", voteItemList);
-            voteBasInfo.put("refdSeriesItemList", sersVoteItemList);
-
-            // voteNum 계산
-            Integer voteNum = getVoteNum(voteBasInfo.getId());
-            voteBasInfo.put("voteNum", (voteNum == null) ? 0 : voteNum);
-
-            // userVoteCnt
-            Integer userVoteCnt = 0;
-            if (mbrId!=null && mbrId.length()>0) {
-                userVoteCnt = getUserVoteCnt(voteBasInfo.getId(), mbrId);
-            }
-            voteBasInfo.put("userVoteCnt", (userVoteCnt == null) ? 0 : userVoteCnt);
-            voteBasInfo.put("userPvCnt", 0);
-            voteBasInfo.put("ipAdrVoteCnt", ipAdrVoteCnt);
-        }
-
-        data.put("items", voteInfoList);
-
-        context.setResult(data);
     }
 
     private Integer getVoteNum(String voteSeq) {
@@ -357,18 +408,18 @@ public class VoteResponseService {
         int totalCount = myVoteList.size();
         int pageSize = 10;
         int page = 1;
-        if(data.get("pageSize") != null){
+        if (data.get("pageSize") != null) {
             int paramPageSize = Integer.parseInt(data.get("pageSize").toString());
             pageSize = paramPageSize;
         }
-        if(data.get("page") != null){
+        if (data.get("page") != null) {
             int paramPage = Integer.parseInt(data.get("page").toString());
             page = paramPage;
         }
         int pageCount = totalCount / pageSize + (totalCount % pageSize > 0 ? 1 : 0);
         List<Map<String, Object>> myVotePagingList = new ArrayList<>();
-        for(int i=(pageSize*(page-1)); i< (page*pageSize) ; i++){
-            if(i <= (myVoteList.size()-1)) {
+        for (int i = (pageSize * (page - 1)); i < (page * pageSize); i++) {
+            if (i <= (myVoteList.size() - 1)) {
                 myVotePagingList.add(myVoteList.get(i));
             }
         }
