e8c0d3c48ab440127aae1b55397512cc6ca32a31
leehh <leehh0713>
2017-10-21 10:01:38 +0900

    [leehh][CJMwave]file 없어도 저장되도록 수정

--- src/main/java/net/ion/ice/cjmwave/mbrInfo/MbrInfoService.java
+++ src/main/java/net/ion/ice/cjmwave/mbrInfo/MbrInfoService.java
@@ -138,7 +138,7 @@ public class MbrInfoService {
         data.put("snsKey", snsKey);
         data.put("mbrDivCd", "2");
         data.put("sttusChgSbst", "정회원 전환");
-        int result = jdbcTemplate.update("update mbrInfo set mbrDivCd='2', aliasNm=?,cntryCd=?,sexCd=?,imgUrl=?,bthYear=?,intrstCdList=?,email=?,emailRcvYn=?, pushMsgRcvYn=?, sbscShapCd=?,infoOttpAgreeYn=?  where snsTypeCd=? and snsKey=? ",snsTypeCd,snsKey,data.get("aliasNm"),data.get("cntryCd"),data.get("sexCd"),data.get("imgUrl"),data.get("bthYear"),data.get("intrstCdList"),data.get("email"),"true".equals(data.get("emailRcvYn")) ? 1:0,"true".equals(data.get("pushMsgRcvYn")) ? 1:0,data.get("sbscShapCd"),"true".equals(data.get("infoOttpAgreeYn")) ? 1:0);
+        int result = jdbcTemplate.update("update mbrInfo set mbrDivCd='2', aliasNm=?,cntryCd=?,sexCd=?,imgUrl=?,bthYear=?,intrstCdList=?,email=?,emailRcvYn=?, pushMsgRcvYn=?, sbscShapCd=?,infoOttpAgreeYn=?  where snsTypeCd=? and snsKey=? ",data.get("aliasNm"),data.get("cntryCd"),data.get("sexCd"),data.get("imgUrl"),data.get("bthYear"),data.get("intrstCdList"),data.get("email"),"true".equals(data.get("emailRcvYn")) ? 1:0,"true".equals(data.get("pushMsgRcvYn")) ? 1:0,data.get("sbscShapCd"),"true".equals(data.get("infoOttpAgreeYn")) ? 1:0,snsTypeCd,snsKey);
         //Node result = (Node) NodeUtils.getNodeService().executeNode(data, "mbrInfo", EventService.UPDATE);
         context.setResult(result);
     }
