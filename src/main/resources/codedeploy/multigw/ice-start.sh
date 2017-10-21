4c7113c073f5f94e26eaf8dfba81201f4d0cb876
jyoh <jyoh@i-on.net>
2017-10-21 20:12:54 +0900

    실수로 지운거 업데이트

--- src/main/resources/codedeploy/multigw/ice-start.sh
+++ src/main/resources/codedeploy/multigw/ice-start.sh
@@ -29,9 +29,6 @@ then
     exit 1
 fi
 
-nohup $JAVA_HOME -jar-Dspring.profiles.active=$PROFILE $WORK_DIR/$WAR_FILE \
-    < /dev/null > $WORK_DIR/ice.log 2>&1 &
-
 echo "Starting application..."
 nohup $JAVA_HOME -jar -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:-CMSParallelRemarkEnabled -Xmx12g -Xms12g -Dspring.profiles.active=$PROFILE $WORK_DIR/$WAR_FILE \
     < /dev/null > $LOG_DIR/ice.log 2>&1 &
