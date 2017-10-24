4eb4716cb081d4c51ef9c9fe86aaec0728a8294c
juneyoungoh <jyoh@i-on.net>
2017-10-24 17:59:22 +0900

    개발 환경 설정 사전 준비

---
+++ src/main/resources/codedeploy/devcache/ice-start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export JAVA_HOME=java
+export APP_HOME=.
+export APP_NAME=ice2-core
+export WAR_FILE=ice2-core-0.0.1-SNAPSHOT.war
+export PROFILE=devcache
+export APP_PORT=8080
+export WORK_DIR=/home/ion/api/core
+
+echo "Service [$APP_NAME] - [$1] START"
+echo "    JAVA_HOME=$JAVA_HOME"
+echo "    APP_HOME=$APP_HOME"
+echo "    APP_NAME=$APP_NAME"
+echo "    WAR_FILE=$WAR_FILE"
+echo "    APP_PORT=$APP_PORT"
+echo "    PROFILE=$PROFILE"
+
+if pkill -0 -f $WORK_DIR/$WAR_FILE > /dev/null 2>&1
+then
+    echo "Service [$APP_NAME] is already running. Ignoring startup request."
+    exit 1
+fi
+
+echo "Starting application..."
+nohup $JAVA_HOME -jar  -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:-CMSParallelRemarkEnabled -Xmx10G -Xms10G -Dspring.profiles.active=$PROFILE $WORK_DIR/$WAR_FILE \
+    1> /dev/null 2>&1 &
+exit 0
---
+++ src/main/resources/codedeploy/devgw/ice-clean.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+export target_dir=/home/ion/api/core
+rm -rf $target_dir
+exit 0
