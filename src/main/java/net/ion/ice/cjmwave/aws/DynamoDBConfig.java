27d20280fe722e163351fc0a07599a3ba8f2eb9b
jaeho <pm1200@i-on.net>
2017-11-11 13:48:36 +0900

    aws dynamo

--- src/main/java/net/ion/ice/cjmwave/aws/DynamoDBConfig.java
+++ src/main/java/net/ion/ice/cjmwave/aws/DynamoDBConfig.java
@@ -1,11 +1,18 @@
 package net.ion.ice.cjmwave.aws;
 
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import net.ion.ice.cjmwave.votePrtcptHst.ArtistVoteStatsByCntryTask;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +25,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories
         (basePackages = "com.baeldung.spring.data.dynamodb.repositories")
 public class DynamoDBConfig {
+    private static Logger logger = LoggerFactory.getLogger(DynamoDBConfig.class);
+
     @Value("${amazon.dynamodb.endpoint}")
     private String amazonDynamoDBEndpoint;
 
@@ -41,7 +50,19 @@ public class DynamoDBConfig {
 
     @Bean
     public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(
-                amazonAWSAccessKey, amazonAWSSecretKey);
+        if(StringUtils.isNotEmpty(this.amazonAWSAccessKey)){
+            return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+        }
+        AWSCredentialsProvider defaultChainProvider = new DefaultAWSCredentialsProviderChain();
+        if (defaultChainProvider.getCredentials() == null) {
+            logger.debug("Loading security credentials from default credentials provider chain.");
+            throw new AmazonClientException("Unable to find AWS security credentials.  "
+                    + "Searched JVM system properties, OS env vars, and EC2 instance roles.  "
+                    + "Specify credentials in Tomcat's context.xml file or put them in one of the places mentioned above.");
+        }
+        logger.debug("Using default AWS credentials provider chain to load credentials");
+        return defaultChainProvider.getCredentials();
+
     }
+
 }
